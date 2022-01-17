8539d2133794549ff499ecbbce8f6d1c2e30b90c
kotouharuto <haruto.k.0321@gmail.com>
2022-01-17 21:53:09 +0900

    sql処理の準備

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from sqlite3.dbapi2 import connect
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,5 +10,18 @@ def home_page():
  msg = 'あなたのフィットネスを少しでも楽しくします'
 
  return render_template('home.html', title=title, msg=msg)
+
+@app.route('/login')
+def login():
+ dbname = ('db/user.db')
+
+ return render_template('login.html')
+
+@app.route('/newac')
+def newac():
+ dbname = ('db/user.db')
+ conn = sqlite3.connect(dbname, isolation_level=None)
+
+ return render_template('newac.html')
 
 app.run(port=8000, debug=True)
